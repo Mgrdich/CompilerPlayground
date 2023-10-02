d1bf6ce95857a9fdd700910c2af955c54e79d275
Mgrdich <mgotm13@gmail.com>
2023-10-02 18:38:21 +0400

    tokens code modifications

--- go/token/token.go
+++ go/token/token.go
@@ -1,5 +1,7 @@
 package token
 
+import "unicode"
+
 // Token is the set of lexical tokens of our created language
 type Token int
 
@@ -13,6 +15,7 @@ const (
 	//  Identifiers and basic type literals
 	// (these tokens stand for classes of literals)
 	literal_beg
+	IDENT
 	INTEGER // 12345
 	STRING  // "abc"
 	literal_end
@@ -21,9 +24,9 @@ const (
 	VAR // var
 	BEGIN
 	END
-	DOT
 	keyword_end
 
+	// Operators and delimiters
 	operator_beg
 	ADD
 	SUB
@@ -31,6 +34,8 @@ const (
 	QUO
 	REM
 	DEFINE
+	DOT
+	SEMICOLOUMN
 	operator_end
 )
 
@@ -39,18 +44,68 @@ var tokens = [...]string{
 	ILLEGAL: "ILLEGAL",
 	COMMENT: "COMMENT",
 
+	IDENT:   "IDENT",
 	INTEGER: "INTEGER",
 	STRING:  "STRING",
 
 	VAR:   "VAR",
 	BEGIN: "BEGIN",
 	END:   "END",
-	DOT:   ".",
 
-	ADD:    "+",
-	SUB:    "-",
-	MUL:    "*",
-	QUO:    "/",
-	REM:    "%",
-	DEFINE: ":=",
+	ADD:         "+",
+	SUB:         "-",
+	MUL:         "*",
+	QUO:         "/",
+	REM:         "%",
+	DEFINE:      ":=",
+	DOT:         ".",
+	SEMICOLOUMN: ";",
+}
+
+var keywords map[string]Token
+
+func init() {
+	keywords = make(map[string]Token, keyword_end-(keyword_beg+1))
+	for i := keyword_beg + 1; i < keyword_end; i++ {
+		keywords[tokens[i]] = i
+	}
+}
+
+// Lookup maps an identifier to its keyword token or IDENT (if not a keyword).
+func Lookup(ident string) Token {
+	if tok, isKeyword := keywords[ident]; isKeyword {
+		return tok
+	}
+	return IDENT
+}
+
+// IsLiteral returns true for tokens corresponding to identifiers
+// and basic type literals; it returns false otherwise.
+func (tok Token) IsLiteral() bool { return literal_beg < tok && tok < literal_end }
+
+// IsOperator returns true for tokens corresponding to operators and
+// delimiters; it returns false otherwise.
+func (tok Token) IsOperator() bool {
+	return operator_beg < tok && tok < operator_end
+}
+
+// IsKeyword reports whether name is a Go keyword, such as "func" or "return".
+func IsKeyword(name string) bool {
+	_, ok := keywords[name]
+	return ok
+}
+
+// IsIdentifier reports whether name is a Go identifier, that is, a non-empty
+// string made up of letters, digits, and underscores, where the first character
+// is not a digit. Keywords are not identifiers.
+func IsIdentifier(name string) bool {
+	if name == "" || IsKeyword(name) {
+		return false
+	}
+	for i, c := range name {
+		if !unicode.IsLetter(c) && c != '_' && (i == 0 || !unicode.IsDigit(c)) {
+			return false
+		}
+	}
+	return true
 }
